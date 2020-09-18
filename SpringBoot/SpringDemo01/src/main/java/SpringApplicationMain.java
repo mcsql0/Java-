134fe6eec6558a3622f04a3a4441987c528214cd
mcsql0 <2697287132@qq.com>
2020-09-18 14:08:51 +0800

    SpringDemo01

---
+++ SpringBoot/SpringDemo01/src/main/java/SpringApplicationMain.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class SpringApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationMain.class,args);
+    }
+}
