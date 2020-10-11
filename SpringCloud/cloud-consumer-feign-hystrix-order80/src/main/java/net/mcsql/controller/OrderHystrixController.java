bbe32380934a13a7a132a10a804ef02b96821d35
mcsql0 <2697287132@qq.com>
2020-10-11 22:18:01 +0800

    2020-10-11 任务提交

---
+++ SpringCloud/cloud-consumer-feign-hystrix-order80/src/main/java/net/mcsql/controller/OrderHystrixController.java
@@ -0,0 +1,52 @@
+package net.mcsql.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import net.mcsql.service.PaymentHystrixService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+// DefaultProperties 需要配合 @HystrixCommand 使用
+public class OrderHystrixController {
+
+    @Autowired
+    PaymentHystrixService paymentHystrixService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @HystrixCommand
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        System.out.println("*******result:"+result);
+        return result;
+    }
+
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+    })
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        System.out.println("*******result:"+result);
+        return result;
+    }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
+    }
+
+}
---
+++ SpringCloud/cloud-gateway-gateway9527/src/main/java/net/mcsql/GateWayMain9527.java
@@ -0,0 +1,12 @@
+package net.mcsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
