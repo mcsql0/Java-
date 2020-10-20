b589b3ed4eda6b55dff1494f6c36ca900b7140bf
mcsql0 <2697287132@qq.com>
2020-10-20 20:04:25 +0800

    2020-10-20 任务提交

---
+++ MQ/ActiveMQAPI/src/main/java/JmsProduce.java
@@ -0,0 +1,35 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class JmsProduce {
+
+    public static final String ACTIVENQ_URL = "tcp://103.152.171.252:61616";
+    public static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws JMSException {
+        //1. 创建链接工厂，按照给定的url地址，采用默认的用户名和密码
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVENQ_URL);
+        //2. 通过连接工厂，获得连接connection并启动访问
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        //3. 创建会话session
+        // 两个参数，第一个叫事务/第二叫签收
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4. 创建目的地(具体是队列还是主题topic)
+        Queue queue = session.createQueue(QUEUE_NAME);
+        //5. 创建消息的生产者
+        MessageProducer producer = session.createProducer(queue);
+        //6. 通过使用MessageProducer产生3条消息发送到MQ的队列里面
+        for (int i = 0; i < 3; i++) {
+            //7. 创建消息，好比学生按照
+            TextMessage textMessage = session.createTextMessage("msg---" + i);
+            //8. 通过messaProducer发送给mq
+            producer.send(textMessage);
+        }
+        //9. 关闭资源
+        producer.close();
+        session.close();
+        connection.close();
+    }
+}
