7b093d52a1ba1ece158917f8abd3b9cbc5862b77
mcsql0 <2697287132@qq.com>
2020-10-28 20:05:36 +0800

    2020-10-28 任务提交

---
+++ mybatis-plus/src/test/java/net/mcsql/mybatisplus/MybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mcsql.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
